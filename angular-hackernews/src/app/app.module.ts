098b015b10b013bb201f76685ac45a83124fe9f6
Hieu.Le <letrunghieu2602@gmail.com>
2020-07-30 15:09:47 +0700

    tách riêng và add footer,menu,content

--- angular-hackernews/src/app/app.module.ts
+++ angular-hackernews/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {FormsModule} from '@angular/forms';
+import { MenuComponent } from './header/menu/menu.component';
+import { ArticlesComponent } from './core/articles/articles.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    ArticlesComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
